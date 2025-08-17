2102d15b4f8053b949f851125917772a0c8555b6
Tsirimaholy <hei.tsirimaholy@gmail.com>
2025-08-17 18:01:28 +0300

    configure font

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className='text-2xl font-bold underline'>Hello world</h1>
+      <h1 className='text-2xl font-bold underline font-geist'>Hello world</h1>
     </div>
   )
 }
